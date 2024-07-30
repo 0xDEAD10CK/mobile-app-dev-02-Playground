e99ac34a4fb267d0ea022f34f33c7d3b3cec3a1b
JoshMBN <joshua.brennan.niu@gmail.com>
2024-07-31 08:47:48 +1200

    Added icons to the navigation using Material Community Icons

--- App.jsx
+++ App.jsx
@@ -1,28 +1,41 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
 
 import HomeScreen from './screens/HomeScreen';
 import DetailsScreen from './screens/DetailsScreen';
 
-const Stack = createNativeStackNavigator();
+//const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: "My Home"}}
+          options={{ 
+            title: "My Home",
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size}) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
         />
-        <Stack.Screen
+        <Tab.Screen
           name="Details"
           component={DetailsScreen}
-          options={({ route }) => ({
-            title: `${route.params.firstName} ${route.params.lastName}`
-          })}
+          options={{
+            title: "My Details",
+            tabBarLabel: "Details",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="card-account-details-outline" color={color} size={size} />
+            ),
+          }}
         />
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
